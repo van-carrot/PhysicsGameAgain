7afa065b112201312434c448ed587ec5c46f0fb0
Aly <marvelousflash@yahoo.com>
2022-11-28 00:09:39 -0500

    Made Opening Story Scene 0, Changed Art in Scene 1

---
+++ FreeFood2D/Assets/Scripts/Screenshot.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screenshot : MonoBehaviour
+{
+    [SerializeField]
+    private string path;
+    private string fileName;
+    [SerializeField]
+    [Range(1, 5)]
+    private int size = 1;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            fileName = "screenshot ";
+            fileName += System.Guid.NewGuid().ToString() + ".png";
+ 
+            ScreenCapture.CaptureScreenshot(path + fileName, size);
+        }
+    }
+}

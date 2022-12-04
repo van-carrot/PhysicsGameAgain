0588c2c0e8bf4dac6031221616deb6fee196bcd2
Attilio Rigotti <arigottijr@gmail.com>
2022-12-04 11:29:11 -0500

    Added a motor and more fun responsive controls to first balancing. Have fun with it!

--- FreeFood2D/Assets/Scripts/InputManager.cs
+++ FreeFood2D/Assets/Scripts/InputManager.cs
@@ -5,23 +5,33 @@ using UnityEngine;
 public class InputManager : MonoBehaviour
 {
     public GameObject foodPlate;
-    public float maganitude;
-    public Vector3 forceRightPos;
-    public Vector3 forceRightVec;
-    public Vector3 forceLeftVec;
-    public Vector3 forceLeftPos;
+    public float magnitude;
+    private HingeJoint2D balanceBoard;
+    //public Vector3 forceRightPos;
+    //public Vector3 forceRightVec;
+    //public Vector3 forceLeftVec;
+    //public Vector3 forceLeftPos;
 
-    public float spd;
+    //public float spd;
 
+    private void Start()
+    {
+        balanceBoard = foodPlate.GetComponent<HingeJoint2D>();
+    }
     private void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
+            balanceBoard.useMotor = true;
+
+            JointMotor2D motor = new JointMotor2D { motorSpeed = magnitude, maxMotorTorque = 10000 };
+            balanceBoard.motor = motor;
+
             //foodPlate.GetComponent<Transform>().Rotate(Vector3.forward, rotateAngle);
 
 
             //Rotation Bowl
-            foodPlate.GetComponent<Rigidbody2D>().AddForceAtPosition(forceLeftVec * maganitude, forceLeftPos);
+            //foodPlate.GetComponent<Rigidbody2D>().AddForceAtPosition(forceLeftVec * magnitude, forceLeftPos);
 
             //Moving from right to left
             //foodPlate.transform.position += Vector3.left * spd * Time.deltaTime;
@@ -29,13 +39,21 @@ public class InputManager : MonoBehaviour
         }
         else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-
+            balanceBoard.useMotor = true;
+            JointMotor2D motor = new JointMotor2D { motorSpeed = -magnitude, maxMotorTorque = 10000 };
+            balanceBoard.motor = motor;
             //Rotation Bowl
-            foodPlate.GetComponent<Rigidbody2D>().AddForceAtPosition(forceRightVec * -1 * maganitude, forceRightPos);
+            //foodPlate.GetComponent<Rigidbody2D>().AddForceAtPosition(forceRightVec * -1 * magnitude, forceRightPos);
 
             //Moving from left to right
             //foodPlate.transform.position += Vector3.right * spd * Time.deltaTime;
 
+        }
+        else
+        {
+            JointMotor2D motor = new JointMotor2D { motorSpeed = 0, maxMotorTorque = 10000 };
+            balanceBoard.motor = motor;
+            balanceBoard.useMotor = false;
         }
     }
 }

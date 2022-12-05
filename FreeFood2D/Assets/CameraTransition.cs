bf58effd30cbbf8fec0ce5995ba7642bf1885bc3
Attilio Rigotti <arigottijr@gmail.com>
2022-12-05 08:24:38 -0500

    Working through camera controller and animations

---
+++ FreeFood2D/Assets/CameraTransition.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTransition : MonoBehaviour
+{
+    public Camera cam;
+    float timeElapsed;
+    public float lerpDuration;
+    public float desiredSize;
+    private bool cameraTrigger;
+    Vector3 startPos;
+    Vector3 endPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = new Vector3(0f, 0f, -9.45f);
+        endPos = new Vector3(28, 10, -9.45f);
+        cam.orthographicSize = 5;
+        cameraTrigger = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            cameraTrigger = true;
+
+        }
+        CameraMove();
+    }
+
+    private void CameraMove()
+    {
+        if (cameraTrigger)
+        {
+            if (timeElapsed < lerpDuration)
+            {
+
+                cam.orthographicSize = Mathf.Lerp(5, desiredSize, timeElapsed / lerpDuration);
+                cam.transform.position = new Vector3(
+                                     Mathf.Lerp(startPos.x, endPos.x, timeElapsed / lerpDuration),
+                                     Mathf.Lerp(startPos.y, endPos.y, timeElapsed / lerpDuration),
+                                      -9.45f);
+                timeElapsed += Time.deltaTime;
+
+            }
+
+        }
+        
+    }
+}
